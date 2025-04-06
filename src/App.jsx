4d054c5100369d07814159dbf4131e39a5e1df39
Jime Matthies <jimematthies@gmail.com>
2025-04-06 00:55:17 -0400

    Install Prettier & Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="app px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-			<Navbar />
-		</div>
-	);
+  return (
+    <div className="app px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-	return (
-		<div className="nb-c w-full h-16 md:h-20 flex items-center justify-between">
-			<div className="nb-l flex items-center gap-4 text-2xl font-bold">
-				<img src={JeanLogo} alt="Logo" className="nb-li w-auto h-8" />
-				<span>Jean's Journal</span>
-			</div>
-			<div className="nb-m md:hidden">
-				<FontAwesomeIcon icon={faBars} />
-			</div>
-			<div className="nb-d hidden md:flex">
-				<FontAwesomeIcon icon={faXmark} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="nb-c flex h-16 w-full items-center justify-between md:h-20">
+      <div className="nb-l flex items-center gap-4 text-2xl font-bold">
+        <img src={JeanLogo} alt="Logo" className="nb-li h-8 w-auto" />
+        <span>Jean's Journal</span>
+      </div>
+      <div className="nb-m md:hidden">
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      <div className="nb-d hidden md:flex">
+        <FontAwesomeIcon icon={faXmark} />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
